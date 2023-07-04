044508192ae76bb475251e20dad426aacd6beec7
4e1ik <Artemi324@tut.by>
2023-07-04 23:23:00 +0300

    init(04.07.2023) add animation for numbers on the main page

--- src/scripts/swiper-product.js
+++ src/scripts/swiper-product.js
@@ -2,11 +2,6 @@ const swiper_product = new Swiper('.swiper-product',{
     direction: 'horizontal',
     loop: true,
 
-    autoplay: {
-        delay:2000,
-        // pauseOnMouseEnter: true,
-    },
-
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
